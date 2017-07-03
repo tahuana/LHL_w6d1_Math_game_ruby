64524c939213ba2c9ace1a4a1a4edab64ac91630
tahuana <tahuana@gmail.com>
2017-07-03 23:14:51 +0000

    changing some variables

--- game.rb
+++ game.rb
@@ -5,18 +5,18 @@ class Game
   end
 
   def play_game
-    @player1 = Player.new
-    @player1.get_name
-    @player2 = Player.new
-    @player2.get_name
+    player1 = Player.new
+    player1.get_name
+    player2 = Player.new
+    player2.get_name
 
     while !@winner
-      turn = Turn.new(@player1, @player2)
+      turn = Turn.new(player1, player2)
       turn.play_turn
 
-      p1 = @player1.total_of_life
-      p2 = @player2.total_of_life
-      puts "#{@player1.name}: #{p1}/3 vs #{@player2.name}: #{p2}/3"
+      p1 = player1.total_of_life
+      p2 = player2.total_of_life
+      puts "#{player1.name}: #{p1}/3 vs #{player2.name}: #{p2}/3"
 
       if p1.to_i == 0 && p2.to_i == 0
         puts "\n---> No winner. Try again. <---"
@@ -24,10 +24,10 @@ class Game
       end
 
       if p1.to_i == 0
-        puts "\n---> #{@player2.name} wins with a score of #{p2}/3 <---"
+        puts "\n---> #{player2.name} wins with a score of #{p2}/3 <---"
         @winner = true
       elsif p2.to_i == 0
-        puts "\n---> #{@player1.name} wins with a score of #{p1}/3 <---"
+        puts "\n---> #{player1.name} wins with a score of #{p1}/3 <---"
         @winner = true
       end
     end
---
+++ math_game.rb
@@ -0,0 +1,7 @@
+require './player'
+require './question'
+require './turn'
+require './game'
+
+game = Game.new
+game.play_game
