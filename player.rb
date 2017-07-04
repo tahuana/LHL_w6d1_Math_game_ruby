a37fa72c3ff25b693f23847e102bcf7f103d6cd5
tahuana <tahuana@gmail.com>
2017-07-04 00:01:12 +0000

    including self in some variables

--- player.rb
+++ player.rb
@@ -1,19 +1,19 @@
 class Player
 
-  attr_reader :total_of_life, :name
+  attr_accessor :total_of_life, :name
 
   def initialize
-    @name = ""
-    @total_of_life = 3
+    self.name = ""
+    self.total_of_life = 3
   end
 
   def get_name
     puts "What is your name?"
-    @name = gets.chomp
+    self.name = gets.chomp
   end
 
   def lose_life
-    @total_of_life -= 1
+    self.total_of_life -= 1
   end
 
 end
